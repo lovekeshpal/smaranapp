427be959417c50e9c0cb0515d139693ebfb3f315
Lovekesh Pal <lovekeshfgr@gmail.com>
2026-03-31 10:29:07 +0530

    Port Change

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
   res.send("Backend running 🚀");
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
